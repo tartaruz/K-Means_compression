f6679e3661e79dae770a3703d18e4b94012645a8
Thomas Ramirez <thomas.ramirez@blank.no>
2023-10-17 20:17:29 +0200

    Added position importance

--- k-compress.py
+++ k-compress.py
@@ -1,8 +1,10 @@
 from sklearn.cluster import KMeans
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import random
+
+
 class K_compress:
     def __init__(self, filename=None):
         self.filename = filename
@@ -26,29 +28,34 @@ class K_compress:
                 print("Not PNG")
         except:
             print("Error getting imagen")
-        
+
     def setSize(self):
         self.size = self.img.size
 
     def extractData(self):
         for x in range(self.size[0]):
             for y in range(self.size[1]):
-                self.data.append(list(self.pix[x,y]))
+                data = list(self.pix[x, y])
+                norm_x = (x/self.size[0]*255*importance)
+                norm_y = (y/self.size[1]*255*importance)
+                data.append(norm_x)
+                data.append(norm_y)
+                self.data.append(data)
 
-        
-    def compress(self, cluster=16):  
+    def compress(self, cluster=16):
         self.getImagen()
         self.setSize()
-        self.extractData()  
+        self.extractData()
         kmean = KMeans(n_clusters=cluster)
         kmean.fit(self.data)
-        center, point = [cent for cent in kmean.cluster_centers_.astype(int)], 0
+        center, point = [
+            cent for cent in kmean.cluster_centers_.astype(int)], 0
         for x in range(self.size[0]):
             for y in range(self.size[1]):
-                new_color = center[kmean.labels_[point]]
+                new_color = center[kmean.labels_[point]][:-2]
                 new_color = tuple(new_color.tolist())
-                self.pix[x,y] = new_color
-                point+=1
+                self.pix[x, y] = new_color
+                point += 1
                 self.newData.append(new_color)
                 key = str(list(new_color))
         newName = self.filename.split(".")[0]+"["+str(cluster)+"_colors]"
@@ -58,5 +65,6 @@ class K_compress:
 
 filename = input("Filename: ")
 k_value = int(input("K_value: "))
-r = K_compress(filename = filename)
+importance = float(input("Importance(0.0-1.0): "))
+r = K_compress(filename=filename)
 r.compress(cluster=k_value)
